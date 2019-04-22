e7a4cabe66275ab9925d011ca824ae4297525442
Miguel Gomez Jr <miguel.gomezjr91@gmail.com>
2019-04-22 12:27:01 -0700

    First Commit

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -8,8 +8,39 @@ namespace Lab2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Enter Length");
+            decimal length = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Width");
+            decimal width = decimal.Parse(Console.ReadLine());
+
+            decimal area = length * width;
+            decimal perimeter = (2 * length) + (2 * width);
+
+            Console.WriteLine("Area: {0}",area);
+            Console.WriteLine("Perimeter: {0}", perimeter);
+
+            Console.WriteLine("Do you want to continue? (Y/N)");
+            string input = Console.ReadLine();
+
+            if (input == "Y" || input == "y")
+            {
+                Console.WriteLine("Enter Height");
+                decimal height = decimal.Parse(Console.ReadLine());
+
+                decimal volume = length * width * height;
+
+                Console.WriteLine("Volume: {0}", volume);
+
+            }
+            else if (input == "N" || input == "n")
+                    {
+                Console.WriteLine("Well have a nice day.");
+            }
+
+
+
         }
     }
 }
